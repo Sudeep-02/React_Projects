8743eef0e9388b3c83eabd8125671d86dfe49df5
Sudeep <Sudeep.chavan2000@gmail.com>
2024-07-02 17:32:43 +0530

    Background Chnage

---
+++ BackgroundChange/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function App() {
+  const [color,setColor] = useState('red')
+ 
+  const handleColor = () => {
+   const characterSet = "0123456789ABCDEF";
+   let newHexColor = "";
+
+       for (let i = 0; i < 6; i++) {
+           let random = Math.floor(Math.random() * 16);
+           newHexColor += characterSet[random];
+           }
+          //  console.log(newHexColor);
+      setColor("#"+newHexColor);    
+
+  }
+
+  return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
+    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+     <div className="flex justify-center shadow-lg bg-black  px-3 py-2 rounded-3xl">
+      <button onClick={handleColor} className="outline-none px-4 py-1 rounded-full text-white shadow-sm shadow-orange-400 hover:shadow-inner hover:shadow-cyan-200">Random Color</button>
+     </div>
+    </div>
+    </div>
+     
+    
+  )
+}
+
+export default App
